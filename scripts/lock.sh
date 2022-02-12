c8fcb1944331c376f8e0a2d61d2306d2b70fae63
JackieMium <jiangjun12321@gmail.com>
2022-02-12 15:24:25 +0800

    small tweaks

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -10,38 +10,39 @@
 rectangles=" "
 case `uname` in
   (Linux)
-    font="Comic Sans MS"
+    font="Comic Sans MS:style=Bold"
     ;;
   (FreeBSD)
-    font="Comic Neue"
+    font="Comic Neue:style=Bold Italic"
     ;;
 esac
 
 SR=$(xrandr --query | grep ' connected' | grep -o '[0-9][0-9]*x[0-9][0-9]*[^ ]*')
 for RES in $SR; do
   SRA=(${RES//[x+]/ })
-  CX=$((${SRA[2]} + 25))
-  CY=$((${SRA[1]} - 80))
-  rectangles+="rectangle $CX,$CY $((CX+300)),$((CY-80)) "
+  CX=$((${SRA[2]} + 70))
+  CY=$((${SRA[1]} - 580))
+  rectangles+="rectangle $CX,$CY $((CX+450)),$((CY-150)) "
 done
 
 TMPBG=/tmp/screen.png
-scrot $TMPBG && convert $TMPBG -scale 5% -scale 2000% -draw "fill black fill-opacity 0.4 $rectangles" $TMPBG
+scrot $TMPBG && convert $TMPBG -scale 5% -scale 2000% -draw "fill black fill-opacity 0.5 $rectangles" $TMPBG
 
 i3lock \
   -i $TMPBG \
   --time-str="%H:%M" \
-  --clock --date-str="%A, %Y-%m-%d" \
+  --clock --date-str="%a, %Y-%m-%d" \
   --greeter-text="Type password to unlock..." \
-  --verif-text="..." --wrong-text="Nah..." \
-  --greeter-pos="x+300:y+710" \
-  --time-pos="x+150:y+760" \
-  --date-pos="tx+25:ty+25"  --ind-pos="tx+340:ty+750" \
+  --verif-text="..." --wrong-text="Wrong Password!" \
+  --time-pos="x+135:y+460" \
+  --date-pos="tx+15:ty+30" \
+  --greeter-pos="x+300:y+410" \
+  --ind-pos="tx+340:ty+458" \
   --inside-color="00000000" --ring-color="ffffffff" --line-uses-inside \
   --keyhl-color="d23c3dff" --bshl-color="d23c3dff" --separator-color="00000000" \
   --insidever-color="fecf4dff" --insidewrong-color="d23c3dff" \
   --ringver-color="ffffffff" --ringwrong-color="ffffffff" \
-  --radius=25 --ring-width=5 \
+  --radius=25 --ring-width=8 \
   --time-font="$font" --date-font="$font" --layout-font="$font" \
   --greeter-font="$font" --verif-font="$font" --wrong-font="$font" \
   --verif-color="ffffffff" --greeter-color="#ff33cc" \
